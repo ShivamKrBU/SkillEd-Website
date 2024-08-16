532ce32b6a9a2c1166615f1a769c9d0732b0a5b1
ShivamKrBU <shivamkrbu005@gmail.com>
2024-08-16 12:50:58 +0530

    vite

--- MasterjiFrontEnd/vite.config.js
+++ MasterjiFrontEnd/vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'dist', // Example, if index.html is in 'public' directory
   plugins: [react()],
 })
